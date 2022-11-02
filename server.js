37b5e87915790bdd7c2bb27e924491474fddd0bf
Nozay Romain <romain.nozay@gmail.com>
2022-11-02 15:04:58 +0100

    Ajout variable environnement

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const http = require('http');
 //importer application 
 const app = require('./app');
 
+//importer package pour utiliser les variables d'environnement
+const dotenv = require("dotenv");
+const result = dotenv.config();
+
 //Paramètrage du port avec la méthode set de Express
-app.set("PORT, 3000");
+app.set("PORT", process.env.PORT);
 
 //La méthode createServer () prend en argument la focntion qui sera reçu par le server ici les fonction de app.js
 const server = http.createServer(app);
 
 //serveur écoute les requêtes sur le port :
-server.listen(3000);
+server.listen(process.env.PORT);
