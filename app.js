c0d57d440e2105afb2cc20405d64e82feb2698a2
Nozay Romain <romain.nozay@gmail.com>
2022-11-02 15:22:24 +0100

    Ajout de dépendance
    morgan et ajout de statut de réponse

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 //importation de express
 const express = require ("express");
 
+//importation de morgan (logger http)
+const morgan = require("morgan");
+
 //pour créer une application express
 const app = express();
+
+//logger les requête (req) et réponse res
+app.use(morgan("dev"));
 
 //app.use () route générale et la fonction
 app.use((req,res, next) => {
     console.log("première requête 1");
+    next();
+});
+
+//statut de la réponse
+app.use((req,res, next) => {
+    res.status(200);
     next();
 });
 
