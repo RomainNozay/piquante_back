f993c7ad75ca49dcaaad67bb81680ef72a90329d
Nozay Romain <romain.nozay@gmail.com>
2022-11-02 16:23:01 +0100

    création d'un modèle pour enregistrement
    de nouveaux utilisateurs

--- db/db.js
+++ db/db.js
@@ -1,11 +1,13 @@
 //mongodb+srv://Papours:<password>@cluster0.ealogoq.mongodb.net/?retryWrites=true&w=majority
+
 //importer package pour utiliser les variables d'environnemnt
 const dotenv = require("dotenv");
 const result = dotenv.config();
+
 //importer mongoose pour me connecter à la abse de donnée mongoDB
 const mongoose = require('mongoose');
 mongoose.connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ealogoq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log("connexion à mongoDB réussi"))
 .catch(() => console.log("connexion à mongoDB échec"));
 
---
+++ models/users.js
@@ -0,0 +1,12 @@
+//importer mongoose
+const mongoose = require('mongoose')
+
+// le modèle de base de donnée pour le signup 
+const usersSchema = mongoose.Schema({
+    email: { type: String, required: true, unique: true};
+    password: { type: String, required: true}
+});
+
+
+//exportation du module
+module.exports = mongoose.model("user", usersSchema);
