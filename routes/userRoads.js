af1a1a01245d7a3c35bf3021a3078d6789507f69
Nozay Romain <romain.nozay@gmail.com>
2022-11-10 14:27:02 +0100

    ajout de dépendance
    Rate limit limité le nombre de tentative de connexion
    helmet qui sert asécurite les header http

--- routes/userRoads.js
+++ routes/userRoads.js
@@ -1,6 +1,6 @@
 //importation express
 const express = require("express");
-
+const raterLimit = require("express-rate-limit")
 ///importantion du controllers/user.js
 const userControllers = require("../controllers/userControllers");
 //Importation du middleware/password
@@ -8,11 +8,15 @@ const password = require("../middleware/password");
 //Router
 const router = express.Router();
 
+const limiter = raterLimit ({
+    windowMs : 5 * 60 * 1000,
+    max : 5,
+})
 //la route signup (endpoint)
 router.post('/signup', password, userControllers.signup);
 
 //la route login (endpoint)
-router.post("/login", userControllers.login)
+router.post("/login", limiter, userControllers.login)
 
 //exportation du module
 module.exports = router;
